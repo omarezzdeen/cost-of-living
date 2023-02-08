00cf230f24b9bd33b81b99b637fd505b2ef392be
omar ezzdeen <omar.izziddeen@gmail.com>
2023-02-08 22:44:03 +0200

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,14 +6,20 @@ import interactor.GetHighestSalaryAverageCititesNamesInteractor
 
 fun main() {
     val csvParser = CsvParser()
+
     val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser)
 
     val getHighestSalaryAverageCities = GetHighestSalaryAverageCititesNamesInteractor(dataSource)
+
     println(getHighestSalaryAverageCities.execute(limit = 10))
+
     printSeparationLine()
 
+
     val getCityHasCheapestInternetConnectionInteractor = GetCityHasCheapestInternetConnectionInteractor(dataSource)
+
     println(getCityHasCheapestInternetConnectionInteractor.execute())
+
 
 }
 private fun printSeparationLine(){
